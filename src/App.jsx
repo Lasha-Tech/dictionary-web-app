c2d8b0d4a2598bb478b428ff9d72d34b5287e5a3
Lasha Efremidze <lashaefremidze1@gmail.com>
2023-10-01 23:42:21 +0400

    update text content from API server

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,18 @@ function App() {
   const [ballMargin, setBallMargin] = useState('0px');
   const [fontFamilyList, setFontFamilyList] = useState(false);
   const [inputValue, setInputValue] = useState('');
-  const [result, setResult] = useState(true);
+  const [result, setResult] = useState(false);
   const [phonetic, setPhonetic] = useState('');
   const [audioSrc, setAudioSrc] = useState('');
+  const [nounVisibility, setNounVisibility] = useState(false);
+  const [nounArray, setNounArray] = useState();
+  const [synonymVisibility, setSynonymVisibility] = useState(false);
+  const [synonym, setSynonym] = useState();
+  const [verbVisibility, setVerbVisibility] = useState(false);
+  const [verbArray, setVerbArray] = useState();
+  const [sourceVisibility, setSourceVisibility] = useState(false);
+  const [source, setSource] = useState();
+
 
 
   //Ball Click Function
@@ -25,27 +34,60 @@ function App() {
     try {
       const response = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + inputValue)
       // Filter Phonetic Functions
-      const filterPhonetic = response.data[0].phonetics.filter(phonetic => {
+      const filterPhonetic = await response.data[0].phonetics.filter(phonetic => {
         return phonetic.hasOwnProperty('text') === true && phonetic.hasOwnProperty('audio') === true
       })
-      const filterTextAudio = filterPhonetic.filter(phonetic => {
+      const filterTextAudio = await filterPhonetic.filter(phonetic => {
         return phonetic.text !== '' && phonetic.audio !== ''
       })
       setPhonetic(filterTextAudio[0].text)
       setAudioSrc(filterTextAudio[0].audio)
-      console.log(filterTextAudio)
 
+      // Noun Visibility Function 
+      const filterMeaningsNoun = await response.data[0].meanings.filter(meanings => {
+        return meanings.partOfSpeech === 'noun'
+      })
+      if(filterMeaningsNoun.length !== 0) {
+        setNounVisibility(true)
+        if(filterMeaningsNoun[0].definitions.length !== 0) {
+          setNounArray(filterMeaningsNoun[0].definitions)
+        } 
+      } else {
+        setNounVisibility(false)
+      }
 
-      // const phoneticss = response.data[0].phonetics.filter(phon => {
-      //     return phon.audio !== '';
-      // })
+      // Synonym Visibility Function 
+      if(response.data[0].meanings[0].synonyms.length !== 0) {
+        setSynonym(response.data[0].meanings[0].synonyms[0])
+        setSynonymVisibility(true)
+      } else {
+        setSynonymVisibility(false)
+      }
 
-      // const result = phoneticss[0]
+      // Verb Visibility Function
+      const filterMeaningsVerb = await response.data[0].meanings.filter(meanings => {
+        return meanings.partOfSpeech === 'verb'
+      })
+      if(filterMeaningsVerb.length !== 0) {
+        setVerbVisibility(true)
+        if(filterMeaningsVerb[0].definitions.length !== 0) {
+          setVerbArray(filterMeaningsVerb[0].definitions)
+        } 
+      } else {
+        setVerbVisibility(false)
+      }
 
-
-
+      // Source Visibility Function
+      if(response.data[0].sourceUrls.length !== 0) {
+        setSource(response.data[0].sourceUrls[0])
+        setSourceVisibility(true)
+      } else {
+        setSourceVisibility(false)
+      }
     } catch (error) {
-      console.log('egari')
+      setResult(false)
+    } finally {
+      setResult(true)
     }
   }
 
@@ -145,72 +187,83 @@ function App() {
         </div>
 
         {/* Noun  */}
-        <div className="noun-verb">
-          <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>noun</p>
-          <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
-        </div>
-
-        {/* Meaning  List */}
-        {/* Noun Meaning  */}
-        <p style={{fontFamily: fontFamily}} className='meaning'>Meaning</p>
-        <div className='list-div'>
-          <div className="list-item">
-            <li></li>
-            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
-              {}
-              </p>
+        {nounVisibility && 
+        <div className="noun-container">
+          <div className="noun-verb">
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>noun</p>
+            <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
           </div>
 
-          <div className="list-item">
-            <li></li>
-            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
-              {}
-              </p>
+          {/* Meaning  List */}
+          {/* Noun Meaning  */}
+          <p style={{fontFamily: fontFamily}} className='meaning'>Meaning</p>
+          <div className='list-div'>
+          {nounArray.map((definition, index) => {
+            if(index < 3) {
+              return (
+                <div className="list-item" key={Math.random()}>
+                  <li></li>
+                  <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
+                    {definition.definition}
+                    </p>
+                </div>
+              )
+            }
+          })}
           </div>
-
-          <div className="list-item">
-            <li></li>
-            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
-              {}
-              </p>
-          </div>
-        </div>
+        </div>}
+        
 
         {/* Synonym  */}
+        {synonymVisibility && 
         <div className="synonyms-div">
           <p style={{fontFamily: fontFamily}} className='synonym-title'>Synonyms</p>
-          <p className="synonym" style={{fontFamily: fontFamily}}>{}</p>
-        </div>
+          <p className="synonym" style={{fontFamily: fontFamily}}>{synonym}</p>
+        </div>}
 
         {/* Verb  */}
-        <div className="noun-verb">
-          <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>Verb</p>
-          <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
-        </div>
+        {verbVisibility && 
+        <div className="verb-container">
+          <div className="noun-verb">
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>Verb</p>
+            <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
+          </div>
 
-        {/* Verb Meaning  */}
-        <p style={{fontFamily: fontFamily}} className='meaning'>Meaning</p>
-        <div className='list-div'>
-          <div className="list-item">
-            <li></li>
-            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
-              {}
-              </p>
+          {/* Verb Meaning  */}
+          <p style={{fontFamily: fontFamily}} className='meaning'>Meaning</p>
+          <div className='list-div'>
+          {verbArray.map((definition, index) => {
+            if(index < 3) {
+              return (
+                <div className="list-item" key={Math.random()}>
+                  <li></li>
+                  <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
+                    {definition.definition}
+                    </p>
+                </div>
+              )
+            }
+          })}
           </div>
-          </div>
+        </div>
+        }
 
           {/* Footer Border */}
           <hr className='footer-border' style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
 
           {/* Source  */}
-          <p className='source' style={{fontFamily: fontFamily}}>Source</p>
-          <div className="source-div">
-            <a style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}} target='_blank'
-             href={{}}>{}</a>
-            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" fill="none">
-              <path d="M6.09091 4.29545C6.50512 4.29545 6.84091 3.95967 6.84091 3.54545C6.84091 3.13124 6.50512 2.79545 6.09091 2.79545V4.29545ZM1.42603 3.97148L1.95635 4.50182L1.95637 4.50181L1.42603 3.97148ZM1.42603 12.574L1.95638 12.0437L1.95637 12.0436L1.42603 12.574ZM11.2045 7.90909C11.2045 7.49488 10.8688 7.15909 10.4545 7.15909C10.0403 7.15909 9.70455 7.49488 9.70455 7.90909H11.2045ZM4.83331 8.10603C4.54041 8.39893 4.54041 8.8738 4.83331 9.16669C5.1262 9.45959 5.60107 9.45959 5.89397 9.16669L4.83331 8.10603ZM13.1667 1.89397C13.4596 1.60107 13.4596 1.1262 13.1667 0.833306C12.8738 0.540413 12.3989 0.540413 12.106 0.833306L13.1667 1.89397ZM12.6364 2.11364C13.0506 2.11364 13.3864 1.77785 13.3864 1.36364C13.3864 0.949423 13.0506 0.613636 12.6364 0.613636V2.11364ZM9 0.613636C8.58579 0.613636 8.25 0.949423 8.25 1.36364C8.25 1.77785 8.58579 2.11364 9 2.11364V0.613636ZM13.3864 1.36364C13.3864 0.949423 13.0506 0.613636 12.6364 0.613636C12.2221 0.613636 11.8864 0.949423 11.8864 1.36364H13.3864ZM11.8864 5C11.8864 5.41421 12.2221 5.75 12.6364 5.75C13.0506 5.75 13.3864 5.41421 13.3864 5H11.8864ZM6.09091 2.79545H2.45455V4.29545H6.09091V2.79545ZM2.45455 2.79545C1.86987 2.79545 1.30913 3.02771 0.895692 3.44116L1.95637 4.50181C2.08849 4.36968 2.26769 4.29545 2.45455 4.29545V2.79545ZM0.895706 3.44115C0.482259 3.85458 0.25 4.41532 0.25 5H1.75C1.75 4.81314 1.82423 4.63394 1.95635 4.50182L0.895706 3.44115ZM0.25 5V11.5455H1.75V5H0.25ZM0.25 11.5455C0.25 12.1301 0.482269 12.6908 0.895685 13.1043L1.95637 12.0436C1.82422 11.9115 1.75 11.7323 1.75 11.5455H0.25ZM0.895678 13.1043C1.30913 13.5178 1.86988 13.75 2.45455 13.75V12.25C2.26768 12.25 2.08849 12.1758 1.95638 12.0437L0.895678 13.1043ZM2.45455 13.75H9V12.25H2.45455V13.75ZM9 13.75C9.58466 13.75 10.1454 13.5177 10.5588 13.1043L9.49818 12.0436C9.36603 12.1758 9.18683 12.25 9 12.25V13.75ZM10.5588 13.1043C10.9723 12.6908 11.2045 12.1301 11.2045 11.5455H9.70455C9.70455 11.7323 9.63033 11.9115 9.49818 12.0436L10.5588 13.1043ZM11.2045 11.5455V7.90909H9.70455V11.5455H11.2045ZM5.89397 9.16669L13.1667 1.89397L12.106 0.833306L4.83331 8.10603L5.89397 9.16669ZM12.6364 0.613636H9V2.11364H12.6364V0.613636ZM11.8864 1.36364V5H13.3864V1.36364H11.8864Z" fill="#757575"/>
-            </svg>
+          {sourceVisibility && 
+          <div className="source-container">
+            <p className='source' style={{fontFamily: fontFamily}}>Source</p>
+            <div className="source-div">
+              <a style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}} target='_blank'
+              href={source}>{source}</a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" fill="none">
+                <path d="M6.09091 4.29545C6.50512 4.29545 6.84091 3.95967 6.84091 3.54545C6.84091 3.13124 6.50512 2.79545 6.09091 2.79545V4.29545ZM1.42603 3.97148L1.95635 4.50182L1.95637 4.50181L1.42603 3.97148ZM1.42603 12.574L1.95638 12.0437L1.95637 12.0436L1.42603 12.574ZM11.2045 7.90909C11.2045 7.49488 10.8688 7.15909 10.4545 7.15909C10.0403 7.15909 9.70455 7.49488 9.70455 7.90909H11.2045ZM4.83331 8.10603C4.54041 8.39893 4.54041 8.8738 4.83331 9.16669C5.1262 9.45959 5.60107 9.45959 5.89397 9.16669L4.83331 8.10603ZM13.1667 1.89397C13.4596 1.60107 13.4596 1.1262 13.1667 0.833306C12.8738 0.540413 12.3989 0.540413 12.106 0.833306L13.1667 1.89397ZM12.6364 2.11364C13.0506 2.11364 13.3864 1.77785 13.3864 1.36364C13.3864 0.949423 13.0506 0.613636 12.6364 0.613636V2.11364ZM9 0.613636C8.58579 0.613636 8.25 0.949423 8.25 1.36364C8.25 1.77785 8.58579 2.11364 9 2.11364V0.613636ZM13.3864 1.36364C13.3864 0.949423 13.0506 0.613636 12.6364 0.613636C12.2221 0.613636 11.8864 0.949423 11.8864 1.36364H13.3864ZM11.8864 5C11.8864 5.41421 12.2221 5.75 12.6364 5.75C13.0506 5.75 13.3864 5.41421 13.3864 5H11.8864ZM6.09091 2.79545H2.45455V4.29545H6.09091V2.79545ZM2.45455 2.79545C1.86987 2.79545 1.30913 3.02771 0.895692 3.44116L1.95637 4.50181C2.08849 4.36968 2.26769 4.29545 2.45455 4.29545V2.79545ZM0.895706 3.44115C0.482259 3.85458 0.25 4.41532 0.25 5H1.75C1.75 4.81314 1.82423 4.63394 1.95635 4.50182L0.895706 3.44115ZM0.25 5V11.5455H1.75V5H0.25ZM0.25 11.5455C0.25 12.1301 0.482269 12.6908 0.895685 13.1043L1.95637 12.0436C1.82422 11.9115 1.75 11.7323 1.75 11.5455H0.25ZM0.895678 13.1043C1.30913 13.5178 1.86988 13.75 2.45455 13.75V12.25C2.26768 12.25 2.08849 12.1758 1.95638 12.0437L0.895678 13.1043ZM2.45455 13.75H9V12.25H2.45455V13.75ZM9 13.75C9.58466 13.75 10.1454 13.5177 10.5588 13.1043L9.49818 12.0436C9.36603 12.1758 9.18683 12.25 9 12.25V13.75ZM10.5588 13.1043C10.9723 12.6908 11.2045 12.1301 11.2045 11.5455H9.70455C9.70455 11.7323 9.63033 11.9115 9.49818 12.0436L10.5588 13.1043ZM11.2045 11.5455V7.90909H9.70455V11.5455H11.2045ZM5.89397 9.16669L13.1667 1.89397L12.106 0.833306L4.83331 8.10603L5.89397 9.16669ZM12.6364 0.613636H9V2.11364H12.6364V0.613636ZM11.8864 1.36364V5H13.3864V1.36364H11.8864Z" fill="#757575"/>
+              </svg>
+            </div>
           </div>
+          }
       </div> 
       }
     </div>
@@ -218,3 +271,7 @@ function App() {
 }
 
 export default App
+
+
+
+

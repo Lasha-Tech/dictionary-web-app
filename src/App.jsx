d0e04a148e3ea372c4f2a50634b2af5a126d2a16
Lasha Efremidze <lashaefremidze1@gmail.com>
2023-10-01 15:36:50 +0400

    chore: add text content

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { useState } from 'react'
 function App() {
   const [fontFamily, setFontFamily] = useState('sans-serif');
   const [ballMargin, setBallMargin] = useState('0px');
-  const [fontFamilyList, setFontFamilyList] = useState(false)
+  const [fontFamilyList, setFontFamilyList] = useState(false);
   const [inputValue, setInputValue] = useState('');
-  console.log(inputValue)
+  const [result, setResult] = useState(true)
+
   //Ball Click Function
   const handleBallMargin = () => {
     if(ballMargin == '0px') {
@@ -14,8 +15,10 @@ function App() {
     } else {
       setBallMargin('0px')
     }
-  }
+   } // <-- Ball Click Logic: If ball margin equals 0 pixel, the web is in daytime mode, and if it is not equal, it is in nighttime mode.
 
+
+   
   return (
     <div className="App" style={{backgroundColor: ballMargin == '0px'? '#FFF': '#050505'}}>
       {/* Header  */}
@@ -35,6 +38,21 @@ function App() {
             <svg onClick={() => setFontFamilyList(!fontFamilyList)}xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9" fill="none">
               <path d="M1 1L7 7L13 1" stroke="#A445ED" stroke-width="1.5"/>
             </svg>
+
+            {/* Font Family List  */}
+            {fontFamilyList &&
+             <div className="font-family-list" style={{backgroundColor: ballMargin == '0px'? '#FFF': '#1F1F1F',
+            color: ballMargin == '0px'? '#2D2D2D': '#FFFFFF',
+            boxShadow: ballMargin == '0px'? '0px 5px 30px 0px rgba(0, 0, 0, 0.10)': ' 0px 5px 30px 0px #A445ED'}}>
+
+              <p onClick={() => {setFontFamily('sans-serif'); setFontFamilyList(!fontFamilyList)}}
+               style={{fontFamily: 'sans-serif'}} className='sans-serif'>Sans Serif</p>
+              <p onClick={() => {setFontFamily('serif'); setFontFamilyList(!fontFamilyList)}}
+               style={{fontFamily: 'serif'}} className='serif'>Serif</p>
+              <p onClick={() => {setFontFamily('monospace'); setFontFamilyList(!fontFamilyList)}}
+               style={{fontFamily: 'monospace'}} className='mono'>Monospace</p>
+
+            </div>}
           </div>
 
           <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#FFF'}}/> {/* <-- Border  */}
@@ -63,14 +81,6 @@ function App() {
            fontFamily: fontFamily}}/>
         </form>
 
-      {fontFamilyList && <div className="font-family-list" style={{backgroundColor: ballMargin == '0px'? '#FFF': '#1F1F1F',
-        color: ballMargin == '0px'? '#2D2D2D': '#FFFFFF',
-        boxShadow: ballMargin == '0px'? '0px 5px 30px 0px rgba(0, 0, 0, 0.10)': ' 0px 5px 30px 0px #A445ED'}}>
-          <p onClick={() => {setFontFamily('sans-serif'); setFontFamilyList(!fontFamilyList)}} style={{fontFamily: 'sans-serif'}} className='sans-serif'>Sans Serif</p>
-          <p onClick={() => {setFontFamily('serif'); setFontFamilyList(!fontFamilyList)}} style={{fontFamily: 'serif'}} className='serif'>Serif</p>
-          <p onClick={() => {setFontFamily('monospace'); setFontFamilyList(!fontFamilyList)}} style={{fontFamily: 'monospace'}} className='mono'>Mono</p>
-        </div>}
-
         <div className='search-div'>
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
             <path d="M13.193 12.1323C12.9001 11.8394 12.4252 11.8394 12.1323 12.1323C11.8394 12.4252 11.8394 12.9001 12.1323 13.193L13.193 12.1323ZM16.0199 17.0806C16.3128 17.3734 16.7877 17.3734 17.0806 17.0806C17.3734 16.7877 17.3734 16.3128 17.0806 16.0199L16.0199 17.0806ZM2.95195 12.3768L3.48231 11.8465L3.48226 11.8464L2.95195 12.3768ZM5.11403 13.8215L4.82696 14.5144L4.82704 14.5144L5.11403 13.8215ZM10.2147 13.8215L10.5017 14.5144L10.5018 14.5144L10.2147 13.8215ZM12.3768 12.3768L11.8465 11.8464L11.8464 11.8465L12.3768 12.3768ZM13.8215 10.2147L14.5144 10.5018L14.5144 10.5017L13.8215 10.2147ZM12.3768 2.95195L11.8464 3.48228L11.8465 3.4823L12.3768 2.95195ZM12.1323 13.193L16.0199 17.0806L17.0806 16.0199L13.193 12.1323L12.1323 13.193ZM0.25 7.66438C0.25 8.63802 0.441775 9.60213 0.814377 10.5017L2.20021 9.92773C1.90297 9.2101 1.75 8.44103 1.75 7.66438H0.25ZM0.814377 10.5017C1.18699 11.4014 1.73315 12.2187 2.42165 12.9071L3.48226 11.8464C2.93307 11.2973 2.49743 10.6453 2.20021 9.92773L0.814377 10.5017ZM2.4216 12.9071C3.11012 13.5957 3.92748 14.1417 4.82696 14.5144L5.40111 13.1286C4.68347 12.8313 4.03147 12.3957 3.48231 11.8465L2.4216 12.9071ZM4.82704 14.5144C5.72658 14.887 6.69075 15.0788 7.66438 15.0788V13.5788C6.88773 13.5788 6.1186 13.4258 5.40102 13.1286L4.82704 14.5144ZM7.66438 15.0788C8.63803 15.0788 9.60213 14.887 10.5017 14.5144L9.92776 13.1286C9.2101 13.4258 8.44102 13.5788 7.66438 13.5788V15.0788ZM10.5018 14.5144C11.4013 14.1417 12.2187 13.5956 12.9071 12.907L11.8464 11.8465C11.2973 12.3957 10.6453 12.8313 9.92767 13.1286L10.5018 14.5144ZM12.907 12.9071C13.5956 12.2187 14.1417 11.4013 14.5144 10.5018L13.1286 9.92767C12.8313 10.6453 12.3957 11.2973 11.8465 11.8464L12.907 12.9071ZM14.5144 10.5017C14.887 9.60213 15.0788 8.63803 15.0788 7.66438H13.5788C13.5788 8.44102 13.4258 9.2101 13.1286 9.92776L14.5144 10.5017ZM15.0788 7.66438C15.0788 5.69797 14.2977 3.81209 12.9071 2.4216L11.8465 3.4823C12.9557 4.59145 13.5788 6.09578 13.5788 7.66438H15.0788ZM12.9071 2.42163C11.5167 1.03116 9.6308 0.25 7.66438 0.25V1.75C9.23297 1.75 10.7373 2.37312 11.8464 3.48228L12.9071 2.42163ZM7.66438 0.25C5.69796 0.25 3.81209 1.03115 2.42162 2.42162L3.48228 3.48228C4.59145 2.37312 6.09579 1.75 7.66438 1.75V0.25ZM2.42162 2.42162C1.03115 3.81209 0.25 5.69796 0.25 7.66438H1.75C1.75 6.09579 2.37312 4.59145 3.48228 3.48228L2.42162 2.42162Z" fill="#A445ED"/>
@@ -80,8 +90,91 @@ function App() {
       {inputValue == ''? <p className='warning' style={{fontFamily: fontFamily}}>Whoops, can’t be empty…</p>: null}
       </div>
       
-      
+      {result &&
+      <div className="result">
+        <div className="word-result">
+          {/* Word  */}
+          <div className="word-pronounce">
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}} className="word">{}</p>
+            <p className="word-saying-form">{}</p>
+          </div>
 
+          {/* Word Audio  */}
+          <svg className='audio-player' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" fill="none">
+            <circle opacity='0.25' cx="24" cy="24" r="24" fill="#A445ED"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19 18V31L32 24.5L19 18Z" fill="#A445ED"/>
+          </svg>
+        </div>
+
+        {/* Noun  */}
+        <div className="noun-verb">
+          <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>noun</p>
+          <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
+        </div>
+
+        {/* Meaning  List */}
+        {/* Noun Meaning  */}
+        <p style={{fontFamily: fontFamily}} className='meaning'>Meaning</p>
+        <div className='list-div'>
+          <div className="list-item">
+            <li></li>
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
+              {}
+              </p>
+          </div>
+
+          <div className="list-item">
+            <li></li>
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
+              {}
+              </p>
+          </div>
+
+          <div className="list-item">
+            <li></li>
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
+              {}
+              </p>
+          </div>
+        </div>
+
+        {/* Synonym  */}
+        <div className="synonyms-div">
+          <p style={{fontFamily: fontFamily}} className='synonym-title'>Synonyms</p>
+          <p className="synonym" style={{fontFamily: fontFamily}}>{}</p>
+        </div>
+
+        {/* Verb  */}
+        <div className="noun-verb">
+          <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>Verb</p>
+          <hr style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
+        </div>
+
+        {/* Verb Meaning  */}
+        <p style={{fontFamily: fontFamily}} className='meaning'>Meaning</p>
+        <div className='list-div'>
+          <div className="list-item">
+            <li></li>
+            <p style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}}>
+              {}
+              </p>
+          </div>
+          </div>
+
+          {/* Footer Border */}
+          <hr className='footer-border' style={{backgroundColor: ballMargin == '0px'? '#E9E9E9': '#3A3A3A'}}/>
+
+          {/* Source  */}
+          <p className='source' style={{fontFamily: fontFamily}}>Source</p>
+          <div className="source-div">
+            <a style={{fontFamily: fontFamily, color: ballMargin == '0px'? '#2D2D2D': '#FFF'}} target='_blank'
+             href={{}}>{}</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" fill="none">
+              <path d="M6.09091 4.29545C6.50512 4.29545 6.84091 3.95967 6.84091 3.54545C6.84091 3.13124 6.50512 2.79545 6.09091 2.79545V4.29545ZM1.42603 3.97148L1.95635 4.50182L1.95637 4.50181L1.42603 3.97148ZM1.42603 12.574L1.95638 12.0437L1.95637 12.0436L1.42603 12.574ZM11.2045 7.90909C11.2045 7.49488 10.8688 7.15909 10.4545 7.15909C10.0403 7.15909 9.70455 7.49488 9.70455 7.90909H11.2045ZM4.83331 8.10603C4.54041 8.39893 4.54041 8.8738 4.83331 9.16669C5.1262 9.45959 5.60107 9.45959 5.89397 9.16669L4.83331 8.10603ZM13.1667 1.89397C13.4596 1.60107 13.4596 1.1262 13.1667 0.833306C12.8738 0.540413 12.3989 0.540413 12.106 0.833306L13.1667 1.89397ZM12.6364 2.11364C13.0506 2.11364 13.3864 1.77785 13.3864 1.36364C13.3864 0.949423 13.0506 0.613636 12.6364 0.613636V2.11364ZM9 0.613636C8.58579 0.613636 8.25 0.949423 8.25 1.36364C8.25 1.77785 8.58579 2.11364 9 2.11364V0.613636ZM13.3864 1.36364C13.3864 0.949423 13.0506 0.613636 12.6364 0.613636C12.2221 0.613636 11.8864 0.949423 11.8864 1.36364H13.3864ZM11.8864 5C11.8864 5.41421 12.2221 5.75 12.6364 5.75C13.0506 5.75 13.3864 5.41421 13.3864 5H11.8864ZM6.09091 2.79545H2.45455V4.29545H6.09091V2.79545ZM2.45455 2.79545C1.86987 2.79545 1.30913 3.02771 0.895692 3.44116L1.95637 4.50181C2.08849 4.36968 2.26769 4.29545 2.45455 4.29545V2.79545ZM0.895706 3.44115C0.482259 3.85458 0.25 4.41532 0.25 5H1.75C1.75 4.81314 1.82423 4.63394 1.95635 4.50182L0.895706 3.44115ZM0.25 5V11.5455H1.75V5H0.25ZM0.25 11.5455C0.25 12.1301 0.482269 12.6908 0.895685 13.1043L1.95637 12.0436C1.82422 11.9115 1.75 11.7323 1.75 11.5455H0.25ZM0.895678 13.1043C1.30913 13.5178 1.86988 13.75 2.45455 13.75V12.25C2.26768 12.25 2.08849 12.1758 1.95638 12.0437L0.895678 13.1043ZM2.45455 13.75H9V12.25H2.45455V13.75ZM9 13.75C9.58466 13.75 10.1454 13.5177 10.5588 13.1043L9.49818 12.0436C9.36603 12.1758 9.18683 12.25 9 12.25V13.75ZM10.5588 13.1043C10.9723 12.6908 11.2045 12.1301 11.2045 11.5455H9.70455C9.70455 11.7323 9.63033 11.9115 9.49818 12.0436L10.5588 13.1043ZM11.2045 11.5455V7.90909H9.70455V11.5455H11.2045ZM5.89397 9.16669L13.1667 1.89397L12.106 0.833306L4.83331 8.10603L5.89397 9.16669ZM12.6364 0.613636H9V2.11364H12.6364V0.613636ZM11.8864 1.36364V5H13.3864V1.36364H11.8864Z" fill="#757575"/>
+            </svg>
+          </div>
+      </div> 
+      }
     </div>
   )
 }
